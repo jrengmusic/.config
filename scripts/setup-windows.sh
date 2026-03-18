26bcedd096fe231aa3de83dccf1353832f8dcd1f
bayu <bayu@jrengmusic.com>
2026-03-18 22:11:12 +0700

    fix(windows): add ripgrep for snacks grep search

--- scripts/setup-windows.sh
+++ scripts/setup-windows.sh
@@ -157,6 +157,7 @@ PACMAN_PKGS=(
     mingw-w64-x86_64-python-pipx  # Mason uses pipx to install cmake-language-server
     mingw-w64-x86_64-gdb          # DAP adapter for standalone debugging (gdb --interpreter=dap)
     mingw-w64-x86_64-fd           # snacks.nvim file finder (find is disabled on Windows by snacks)
+    mingw-w64-x86_64-ripgrep      # snacks.nvim grep search (<leader>fg)
 )
 
 for pkg in "${PACMAN_PKGS[@]}"; do
